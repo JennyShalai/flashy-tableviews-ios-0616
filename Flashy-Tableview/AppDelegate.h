9e1b4f92108670e159132e7428ebdae3249aea60
JennyShalai <jenny.shalai@gmail.com>
2016-07-03 20:27:24 -0400

    init

---
+++ Flashy-Tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flashy Tableview
+//
+//  Created by Jenny on 7/3/16.
+//  Copyright © 2016 FISFlashyTableview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flashy-Tableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flashy Tableview
+//
+//  Created by Jenny on 7/3/16.
+//  Copyright © 2016 FISFlashyTableview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
